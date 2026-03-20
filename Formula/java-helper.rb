1fd812910a33bbfd2b0aff1089bf5fb027e7e49c
Piero Rospigliosi <pierinho13@gmail.com>
2026-03-20 15:41:45 +0100

    version v0.1.2 menu as default

--- Formula/java-helper.rb
+++ Formula/java-helper.rb
@@ -1,8 +1,8 @@
 class JavaHelper < Formula
   desc "CLI for common Maven tasks in local Java projects"
   homepage "https://github.com/pierinho13/java-helper"
-  url "https://github.com/pierinho13/java-helper/releases/download/v0.1.1/java-helper_Darwin_arm64.tar.gz"
-  sha256 "54079367b10838e0152ff33590c1f95d898b04b2d3864f2565039e2636b22cdc"
+  url "https://github.com/pierinho13/java-helper/releases/download/v0.1.2/java-helper_Darwin_arm64.tar.gz"
+  sha256 "aef2cd72b7c6cb35efcd13f1cd2a4edde3e4b54f30478d3bd9abe5bd60444c84"
   license "MIT"
 
   def install
